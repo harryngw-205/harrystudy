d4d0a3f7be37b3ad0bdbb479f46afed370379b2a
harryngw-205 <harryngw.205@gmail.com>
2025-04-30 15:10:30 +0700

    Study-with-harry

---
+++ 28-Tech/28tech.com.vn/templates/app01/assets/js/custom.js
@@ -0,0 +1,9 @@
+// Contact Fixed
+$(document).ready(function() {
+    $('.contact-fixed').click(function() {
+        $('.contact-fixed__list').toggleClass('show');
+        $('.contact-fixed__close').toggleClass('show');
+        $('.contact-fixed__button').toggleClass('show');
+    });
+});
+// End Contact Fixed
